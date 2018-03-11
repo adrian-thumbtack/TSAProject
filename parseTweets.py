1526997be390eb7e7ca3b4f14abf3068425f5a47
adrian-thumbtack <adrthamburaj@gmail.com>
2018-03-11 12:19:03 -0400

    search for keywords

--- parseTweets.py
+++ parseTweets.py
@@ -1,10 +1,26 @@
 from empath import Empath
 lexicon = Empath()
 
-def searchTweets(twts):
+def getScore(twts):
+	negScore = searchForNeg(twts)
+	posScore = searchForPos(twts)
+	print(negScore)
+	print(posScore)
+	print ((negScore - posScore))
+
+def searchForNeg(twts):
+	a = []
 	for tweet in twts:
-		result = lexicon.analyze(tweet, categories=["violence", "hate", "aggression", "crime", "prison", "suffering", "kill", "irritability", 
-		"death", "ridicule", "neglect", "fight", "injury", "rage", "sadness", "torment", "anger", "ugliness", "pain", "negative_emotion"], normalize=True)
-		negEmotion = lexicon.analyze(tweet, categories=["negative_emotion"], normalize=True)
-		print(result)
-		print(negEmotion)
+		negResult = lexicon.analyze(tweet, categories=["violence", "hate", "aggression", "crime", "prison", "suffering", "kill", "irritability", "death", "ridicule", "neglect", "fight", "injury", "rage", "sadness", "torment", "anger", "ugliness", "pain", "negative_emotion"])
+		print(negResult)
+		a.append(sum(negResult.values())/len(negResult))
+	return sum(a)
+		
+def searchForPos(twts):
+	a = []
+	for tweet in twts:
+		posResult = lexicon.analyze(tweet, categories=["cheerfulness", "family", "pride", "leisure", "exercise", "celebration","love", "trust", "politeness", "optimism", "joy", "affection", "friends", "achievement", "contentment", "positive_emotion"])
+		print(posResult)
+		a.append(sum(posResult.values())/len(posResult))
+	return sum(a)
+		

301ed3332518ad2dd069366a767ab0f5f6ed1430
adithyaparams <adithyaparams@gmail.com>
2018-03-11 11:30:26 -0400

    Add function wrappers

--- tweets.py
+++ tweets.py
@@ -1,16 +1,31 @@
 import tweepy
-from tweepy import OAuthHandlerac
+from tweepy import OAuthHandler
+import re
 from unidecode import unidecode
+import sys
+import warnings
 
-consumer_key = 'lBd0fokEEx2ncJCZUEWVLIUN8'
-consumer_secret = 'AcQ2C1idexoxb0msBycqid2H1QbdtKZXRJh0cMPkpUAaKEWrC6'
-access_token = '4142122823-8mPPgiEnFIEK4MMPGtmaJ8bhxY7rBLP0IdrAN5e'
-access_secret = 'MTyQVvSLkq2OUVbHEVXDTIY58eFgLCvRLyZmvOpcCEx0i'
+def initialize():
+    consumer_key = 'lBd0fokEEx2ncJCZUEWVLIUN8'
+    consumer_secret = 'AcQ2C1idexoxb0msBycqid2H1QbdtKZXRJh0cMPkpUAaKEWrC6'
+    access_token = '4142122823-8mPPgiEnFIEK4MMPGtmaJ8bhxY7rBLP0IdrAN5e'
+    access_secret = 'MTyQVvSLkq2OUVbHEVXDTIY58eFgLCvRLyZmvOpcCEx0i'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
 
-api = tweepy.API(auth)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-for status in tweepy.Cursor(api.user_timeline, screen_name='@realDonaldTrump').items():
-    print unidecode(status._json['text']).encode('utf-8')
+    api = tweepy.API(auth)
+    return api
+
+def get_tweets(api, name='realDonaldTrump'):
+    tweets = []
+    for status in tweepy.Cursor(api.user_timeline, screen_name='@{0}'.format(name)).items():
+        tweets.append(unidecode(status._json['text']).encode('utf-8'))
+    return tweets
+
+if __name__ == "__main__":
+    api = initialize()
+    print get_tweets(api)

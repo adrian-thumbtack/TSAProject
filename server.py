d8d1335b779d3c0ea22028de29c01ffb8e01a6ac
Orange Baron <orangebaronroblox@gmail.com>
2018-03-18 14:46:40 -0400

    woo it sends a thing

--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 import socket
 from empath import Empath
 
-def handler(conn):
-	conn.send(bytearray("HTTP/1.1 200 OK\n","utf-8"))
-	conn.send(bytearray("Content-Type: text/html\n\n","utf-8"))
-	text = "HH SPED" #do this from twitter and scheisse
-	d = Empath().analyze(text)
-	depressed = d["sadness"] > d["joy"] #bs af
-	f = open("depression.html","r")
-	conn.send(bytearray(f.read(),"utf-8"))
+filesToSend = {
+	b"style.css":b"css",
+	b"media/header.jpg":b"jpeg"
+}
+defaultFile = [b"depression.html",b"html"]
+def sendFile(conn,filename,type):
+	conn.send(type)
+	conn.send(b"\n\n")
+	f = open(filename,"rb")
+	conn.send(f.read())
 	f.close()
+def handler(conn):
+	data = conn.recv(1024)
+	conn.send(b"HTTP/1.1 200 OK\nContent-type: text/")
+	sentSomething = False
+	for filename,type in filesToSend.items():
+		if data.find(filename) != -1:
+			sendFile(conn,filename,type)
+			sentSomething = True
+	if not sentSomething:
+		sendFile(conn,defaultFile[0],defaultFile[1])
 	conn.close()
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(("",80))
 serv.listen(65)
+processes = []
 while True:
 	conn,_ = serv.accept()
 	handler(conn)

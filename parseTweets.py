785cf6ce7bee1a833e554eb17f758922cfcf808a
adrian-thumbtack <adrthamburaj@gmail.com>
2018-03-18 10:24:38 -0400

    sentiment analysis

--- parseTweets.py
+++ parseTweets.py
@@ -11,16 +11,16 @@ def getScore(twts):
 def searchForNeg(twts):
 	a = []
 	for tweet in twts:
-		negResult = lexicon.analyze(tweet, categories=["violence", "hate", "aggression", "crime", "prison", "suffering", "kill", "irritability", "death", "ridicule", "neglect", "fight", "injury", "rage", "sadness", "torment", "anger", "ugliness", "pain", "negative_emotion"])
+		negResult = lexicon.analyze(tweet, categories=["negative_emotion"])
 		print(negResult)
-		a.append(sum(negResult.values())/len(negResult))
+		a.append(sum(negResult.values())
 	return sum(a)
 		
 def searchForPos(twts):
 	a = []
 	for tweet in twts:
-		posResult = lexicon.analyze(tweet, categories=["cheerfulness", "family", "pride", "leisure", "exercise", "celebration","love", "trust", "politeness", "optimism", "joy", "affection", "friends", "achievement", "contentment", "positive_emotion"])
+		posResult = lexicon.analyze(tweet, categories=["positive_emotion"])
 		print(posResult)
-		a.append(sum(posResult.values())/len(posResult))
+		a.append(sum(posResult.values())
 	return sum(a)
 		

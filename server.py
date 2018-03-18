fc06c5e63b118d413a72bfae0e6c601ebcc3c42f
Orange Baron <orangebaronroblox@gmail.com>
2018-03-18 13:46:01 -0400

    DOUBLE QUOTE SUPREMACY

--- server.py
+++ server.py
@@ -7,13 +7,13 @@ def handler(conn):
 	text = "HH SPED" #do this from twitter and scheisse
 	d = Empath().analyze(text)
 	depressed = d["sadness"] > d["joy"] #bs af
-	f = open("depression.html",'r')
+	f = open("depression.html","r")
 	conn.send(bytearray(f.read(),"utf-8"))
 	f.close()
 	conn.close()
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('',80))
+serv.bind(("",80))
 serv.listen(65)
 while True:
 	conn,_ = serv.accept()
